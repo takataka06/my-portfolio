4870813f320af1532c79b70c36c233f2ce311f58
takataka06 <reyreyrey0110@gmail.com>
2025-09-09 21:17:07 +0900

    Declear sidebar types

---
+++ src/config/sidebar/items.ts
@@ -0,0 +1,25 @@
+import { User, Home, Send,  BicepsFlexed } from "lucide-react"
+
+
+export const items = [
+  {
+    title: "Home",
+    url: "/",
+    icon: Home,
+  },
+  {
+    title: "About me",
+    url: "/about",
+    icon: User,
+  },
+  {
+    title: "Works",
+    url: "/works",
+    icon: BicepsFlexed,
+  },
+  {
+    title: "Contacts",
+    url: "/contacts",
+    icon: Send,
+  },
+]
--- src/lib/sidebardata.ts
+++
@@ -1,7 +0,0 @@
-export const sidebarItems = [
-  { href: "/", label: "Home" },
-  { href: "/about", label: "About" },
-  { href: "/skills", label: "Skills" },
-  { href: "/works", label: "Works" },
-  { href: "/contacts", label: "Contacts" },
-] as const;
